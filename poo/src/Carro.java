9f94fe23a069e4b691c2672d75b35b67ebf4b08f
alexandresantosm <amenezes031@gmail.com>
2022-07-20 17:59:26 -0300

    :sparkles: feat: constructor overload

--- poo/src/Carro.java
+++ poo/src/Carro.java
@@ -2,4 +2,12 @@ public class Carro {
   String cor;
   String modelo;
   int capacidadeDoTanque;
+  
+  public Carro () {}
+
+  public Carro (String cor, String modelo, int capacidadeDoTanque) {
+    this.cor = cor;
+    this.modelo = modelo;
+    this.capacidadeDoTanque = capacidadeDoTanque;
+  }
 }
