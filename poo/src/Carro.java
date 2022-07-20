58100693ea5bf84fb58783559085dc28cb142083
alexandresantosm <amenezes031@gmail.com>
2022-07-20 18:06:46 -0300

    :sparkles: feat: create get/set methods

--- poo/src/Carro.java
+++ poo/src/Carro.java
@@ -10,4 +10,28 @@ public class Carro {
     this.modelo = modelo;
     this.capacidadeDoTanque = capacidadeDoTanque;
   }
+
+  public String getCor() {
+    return cor;
+  }
+
+  public void setCor(String cor) {
+    this.cor = cor;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public int getCapacidadeDoTanque() {
+    return capacidadeDoTanque;
+  }
+
+  public void setCapacidadeDoTanque(int capacidadeDoTanque) {
+    this.capacidadeDoTanque = capacidadeDoTanque;
+  }
 }
