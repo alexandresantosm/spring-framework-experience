e32cf5a68a4f4c35cbb2079cd1400fef4cdcbcf8
alexandresantosm <amenezes031@gmail.com>
2022-07-20 23:40:12 -0300

    :sparkles: feat: create Car object

--- poo/src/App.java
+++ poo/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        var car1 = new Car();
+
+        car1.setColor("Amarelo");
+        car1.setModel("Ferrari");
+        car1.setTankCapacity(65);
+
+        System.out.println(car1.getColor());
+        System.out.println(car1.getModel());
+        System.out.println(car1.getTankCapacity());
+        System.out.println(car1.fullTankValue(7.58));
+
+        System.out.println();
+
+        var car2 = new Car("Banco", "VW Kombi", 45);
+
+        System.out.println(car2.getColor());
+        System.out.println(car2.getModel());
+        System.out.println(car2.getTankCapacity());
+        System.out.println(car2.fullTankValue(7.58));        
+
     }
 }
