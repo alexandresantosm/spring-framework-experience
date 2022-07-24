de1bf4fb2c79dcc4d95f0b82cf39eaeca0c0e6f6
alexandresantosm <amenezes031@gmail.com>
2022-07-24 17:33:00 -0300

    :sparkles: feat: create Course and Mentorship objects

--- bootcamp/src/App.java
+++ bootcamp/src/App.java
@@ -1,5 +1,31 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import domain.Course;
+import domain.Mentorship;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! - This is Bootcamp Java.");
+        
+        var courseJava = new Course();
+        courseJava.setTitle("Curso Java");
+        courseJava.setDescription("Descrição do curso de Java.");
+        courseJava.setWorkload(8);
+
+        var courseJs = new Course();
+        courseJs.setTitle("Curso JavaScript");
+        courseJs.setDescription("Descrição do curso de JavaScript.");
+        courseJs.setWorkload(4);
+
+        var mentorship = new Mentorship();
+        mentorship.setTitle("Princípios da Orientação a Objetos com Java");
+        mentorship.setDescription("Descrição da mentoria de princípios da Orientação a objecos com Java.");
+        mentorship.setDate(LocalDate.now());
+
+        System.out.println(courseJava);
+        System.out.println(courseJs);
+        System.out.println(mentorship);
+
     }
 }
